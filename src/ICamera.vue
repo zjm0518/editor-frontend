89c53dd3fc478cc9e763f73e738757ccebe77798
jingming.zeng <jmzeng_jk@163.com>
2025-03-07 16:46:52 +0800

    CameraCard component with zoom functionality

--- src/ICamera.vue
+++ src/ICamera.vue
@@ -1,97 +1,99 @@
 <template>
-
   <div class="camera-container">
     <div class="camera-video">
-        <camera-card
-      v-for="index in cameraNum"
-      :key="index"
-      :camera-index="index"
-      :ref="el=>getCameraCardRef(el,index)"
-      :camera-type="cameraParamsList[index-1].cameraType"
-      :camera-s-n="cameraParamsList[index-1].cameraSN"
-    ></camera-card>
-
+      <camera-card
+        v-for="index in cameraNum"
+        :key="index"
+        :camera-index="index"
+        :ref="(el) => getCameraCardRef(el, index)"
+        :camera-type="cameraParamsList[index - 1].cameraType"
+        :camera-s-n="cameraParamsList[index - 1].cameraSN"
+      ></camera-card>
     </div>
-    <div class="camera-col" ref="fixedElement">
-        <el-card class="cameraList">
-          相机类型：
+
+      <el-card class="camera-col">
+        <div class="cameraList">
+
+
+        <div class="cameraList1">
+
+          <div>
+        相机类型：
+        <el-select
+          v-model="cameraParamsList[selectedIndex - 1].cameraType"
+          placeholder="请选择"
+          class="cameraSelect"
+          @change="getCameraSNList"
+        >
+          <el-option
+            v-for="item in cameraTypeOptions"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          >
+          </el-option>
+        </el-select>
+        <el-button plain style="margin-left: 5px" @click="addCamera"
+          >添加相机</el-button
+        >
+</div>
+        <div >
+          <span>当前可用相机：</span>
           <el-select
-            v-model="cameraParamsList[selectedIndex-1].cameraType"
+            v-model="cameraParamsList[selectedIndex - 1].cameraSN"
             placeholder="请选择"
             class="cameraSelect"
-            @change="getCameraSNList"
           >
             <el-option
-              v-for="item in cameraTypeOptions"
+              v-for="item in cameraParamsList[selectedIndex - 1]
+                .cameraSNOptions"
               :key="item.value"
               :label="item.label"
               :value="item.value"
             >
             </el-option>
           </el-select>
-          <el-button plain style="margin-left: 5px" @click="addCamera">添加相机</el-button>
-
-          <div >
-            <span>当前可用相机：</span>
-            <el-select
-              v-model="cameraParamsList[selectedIndex-1].cameraSN"
-              placeholder="请选择"
-              class="cameraSelect"
-            >
-              <el-option
-                v-for="item in cameraParamsList[selectedIndex-1].cameraSNOptions"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              >
-              </el-option>
-            </el-select>
-            <el-button plain @click="editCamera" style="margin-left: 5px">{{
-              editCameraText
-            }}</el-button>
-          </div>
-        </el-card>
-        <el-card class="cameraList">
-          <div class="slider">
-            <span style="margin-right: 10px">曝光：</span>
-            <el-slider
-              v-model="cameraParamsList[selectedIndex-1].exposureTime"
-              show-input
-              :min="0"
-              :max="20000"
-              @change="changeExposure"
-              style="flex: 1"
-            ></el-slider>
-          </div>
-          <div class="slider">
-            <span style="margin-right: 10px">增益：</span
-            ><el-slider
-              v-model="cameraParamsList[selectedIndex-1].gain"
-              show-input
-              :min="0"
-              :max="10"
-              @change="changeGain"
-              style="flex: 1"
-            ></el-slider>
-          </div>
-          <el-button plain @click="getSingleImage">单次采集</el-button>
-          <el-button plain @click="showVideo">实时采集</el-button>
-          <el-button plain @click="closeConnection">停止实时采集</el-button>
-        </el-card></div
-      >
+          <el-button plain @click="editCamera" style="margin-left: 5px">{{
+            editCameraText
+          }}</el-button>
+        </div>
+      </div>
+      <div style="width: 40vw;">
+        <div class="slider">
+          <span style="margin-right: 10px">曝光：</span>
+          <el-slider
+            v-model="cameraParamsList[selectedIndex - 1].exposureTime"
+            show-input
+            :min="0"
+            :max="20000"
+            @change="changeExposure"
+            style="flex: 1"
+          ></el-slider>
+        </div>
+        <div class="slider">
+          <span style="margin-right: 10px">增益：</span
+          ><el-slider
+            v-model="cameraParamsList[selectedIndex - 1].gain"
+            show-input
+            :min="0"
+            :max="10"
+            @change="changeGain"
+            style="flex: 1"
+          ></el-slider>
+        </div>
+        <el-button plain @click="getSingleImage">单次采集</el-button>
+        <el-button plain @click="showVideo">实时采集</el-button>
+        <el-button plain @click="closeConnection">停止实时采集</el-button>
+      </div>
     </div>
-
+      </el-card>
+    </div>
 
 </template>
 <script setup lang="ts">
 import { computed, provide, ref } from "vue";
 import CameraCard from "./components/CameraCard.vue";
-import {
-  ElCard,
-  ElSlider,
-  ElSelect,
-  ElOption,
-} from "element-plus";
+import { ElCard, ElSlider, ElSelect, ElOption } from "element-plus";
 import {
   getCameraParams,
   setCameraParam,
@@ -102,11 +104,11 @@ import {
 
 const cameraNum = ref(1);
 const cameraCardRefs = ref<HTMLElement[]>([]); // 存储所有 camera-card 的引用
-const cameraTypePick = computed(()=>{
-  return cameraParamsList.value[selectedIndex.value-1].cameraType
+const cameraTypePick = computed(() => {
+  return cameraParamsList.value[selectedIndex.value - 1].cameraType;
 });
-const cameraSNPick = computed(()=>{
-  return cameraParamsList.value[selectedIndex.value-1].cameraSN
+const cameraSNPick = computed(() => {
+  return cameraParamsList.value[selectedIndex.value - 1].cameraSN;
 });
 const cameraTypeOptions = ref([
   {
@@ -122,11 +124,10 @@ const cameraTypeOptions = ref([
     label: "内窥镜",
   },
 ]);
-const selectedIndex=ref(1)
-provide("selectedIndex",selectedIndex)
-const getCameraCardRef = (el:any,index:number) => {
-
-  if(el) cameraCardRefs.value[index-1]=el
+const selectedIndex = ref(1);
+provide("selectedIndex", selectedIndex);
+const getCameraCardRef = (el: any, index: number) => {
+  if (el) cameraCardRefs.value[index - 1] = el;
 
   return `cameraCard${index}`; // 通过不同的索引来生成唯一的 ref 名
 };
@@ -136,22 +137,23 @@ interface CameraParams {
   cameraSN: string;
   exposureTime: number;
   gain: number;
-  precameraType:string;
-  precameraSN:string;
-  isOpened:boolean;
-  cameraSNOptions:Array<object>
-
+  precameraType: string;
+  precameraSN: string;
+  isOpened: boolean;
+  cameraSNOptions: Array<object>;
 }
-const cameraParamsList = ref<Array<CameraParams>>([{
-  cameraType: "",
-  cameraSN: "",
-  exposureTime: 0,
-  gain: 0,
-  precameraType:"",
-  precameraSN:"",
-  isOpened:false,
-  cameraSNOptions:[]
-}]);
+const cameraParamsList = ref<Array<CameraParams>>([
+  {
+    cameraType: "",
+    cameraSN: "",
+    exposureTime: 0,
+    gain: 0,
+    precameraType: "",
+    precameraSN: "",
+    isOpened: false,
+    cameraSNOptions: [],
+  },
+]);
 const addCamera = () => {
   cameraNum.value++;
   cameraParamsList.value.push({
@@ -159,15 +161,16 @@ const addCamera = () => {
     cameraSN: "",
     exposureTime: 0,
     gain: 0,
-    precameraType:"",
-    precameraSN:"",
-    isOpened:false,
-    cameraSNOptions:[]
-
+    precameraType: "",
+    precameraSN: "",
+    isOpened: false,
+    cameraSNOptions: [],
   });
 };
-const editCameraText = computed(()=>{
-  return cameraParamsList.value[selectedIndex.value-1].isOpened ? "关闭设备" : "打开设备"
+const editCameraText = computed(() => {
+  return cameraParamsList.value[selectedIndex.value - 1].isOpened
+    ? "关闭设备"
+    : "打开设备";
 });
 
 const editCamera = function () {
@@ -188,7 +191,7 @@ const openCamera = function () {
     .then((res) => {
       console.log("res", res);
       //editCameraText.value = "关闭设备";
-      cameraParamsList.value[selectedIndex.value-1].isOpened=true
+      cameraParamsList.value[selectedIndex.value - 1].isOpened = true;
       getCamerap();
     })
     .catch((err) => {
@@ -199,10 +202,10 @@ const closeCamera = function () {
   closeCamera_({
     cameraType: cameraTypePick.value,
     cameraSN: cameraSNPick.value,
-  }).then(res=>{
-    cameraParamsList.value[selectedIndex.value-1].isOpened=false
-    cameraParamsList.value[selectedIndex.value-1].exposureTime=0
-    cameraParamsList.value[selectedIndex.value-1].gain=0
+  }).then((res) => {
+    cameraParamsList.value[selectedIndex.value - 1].isOpened = false;
+    cameraParamsList.value[selectedIndex.value - 1].exposureTime = 0;
+    cameraParamsList.value[selectedIndex.value - 1].gain = 0;
   });
 };
 
@@ -215,8 +218,9 @@ const getCamerap = function () {
       console.log(res);
       //exposureTime.value = res.ExposureTime;
       //gain.value = res.Gain;
-      cameraParamsList.value[selectedIndex.value-1].exposureTime=res.ExposureTime
-      cameraParamsList.value[selectedIndex.value-1].gain=res.Gain
+      cameraParamsList.value[selectedIndex.value - 1].exposureTime =
+        res.ExposureTime;
+      cameraParamsList.value[selectedIndex.value - 1].gain = res.Gain;
     })
     .catch((err) => {
       console.log(err);
@@ -224,27 +228,28 @@ const getCamerap = function () {
 };
 const getCameraSNList = function (value) {
   //cameraSNOptions.value = [];
-  cameraParamsList.value[selectedIndex.value-1].cameraSNOptions=[];
-  cameraParamsList.value[selectedIndex.value-1].cameraSN = "";
-  if (cameraParamsList.value[selectedIndex.value-1].precameraType != "") {
-    closeCamera_({ cameraType: cameraParamsList.value[selectedIndex.value-1].precameraType,
-      cameraSN:cameraParamsList.value[selectedIndex.value-1].precameraSN
-     });
+  cameraParamsList.value[selectedIndex.value - 1].cameraSNOptions = [];
+  cameraParamsList.value[selectedIndex.value - 1].cameraSN = "";
+  if (cameraParamsList.value[selectedIndex.value - 1].precameraType != "") {
+    closeCamera_({
+      cameraType: cameraParamsList.value[selectedIndex.value - 1].precameraType,
+      cameraSN: cameraParamsList.value[selectedIndex.value - 1].precameraSN,
+    });
   }
 
-  cameraParamsList.value[selectedIndex.value-1].precameraType = value;
+  cameraParamsList.value[selectedIndex.value - 1].precameraType = value;
   //editCameraText.value = "打开设备";
   //exposureTime.value = 0;
   //gain.value = 0;
   getCameraSNList_({ cameraType: value }).then((res) => {
-    console.log("res:",res);
+    console.log("res:", res);
 
-    if ( !res) {
-      return
+    if (!res) {
+      return;
     }
     const SNList = res.cameraSN;
     for (const sn of SNList) {
-      cameraParamsList.value[selectedIndex.value-1].cameraSNOptions.push({
+      cameraParamsList.value[selectedIndex.value - 1].cameraSNOptions.push({
         value: sn,
         label: sn,
       });
@@ -280,60 +285,57 @@ const changeGain = function (newvalue) {
   });
 };
 const getSingleImage = function () {
-  cameraCardRefs.value[selectedIndex.value-1].getSingleImage();
+  cameraCardRefs.value[selectedIndex.value - 1].getSingleImage();
 };
 const showVideo = function () {
-  cameraCardRefs.value[selectedIndex.value-1].showVideo();
+  cameraCardRefs.value[selectedIndex.value - 1].showVideo();
 };
 const closeConnection = function () {
-  cameraCardRefs.value[selectedIndex.value-1].closeConnection();
-};
-/* const fixedElement = ref(null);
-
-const updateWidth = () => {
-  const parentWidth = fixedElement.value?.parentElement.offsetWidth || 0;
-  if (fixedElement.value) {
-    fixedElement.value.style.width = `${parentWidth * 0.4}px`; // 设置宽度为父元素的40%
-  }
+  cameraCardRefs.value[selectedIndex.value - 1].closeConnection();
 };
 
-onMounted(() => {
-  updateWidth(); // 初始加载时设置宽度
-  window.addEventListener('resize', updateWidth); // 监听窗口大小变化
-}); */
 </script>
 <style scoped>
 .camera-container {
   display: flex;
   justify-content: center;
   align-items: center;
-  overflow: auto;
+  overflow: hidden;
   /* position: relative; */
   flex-direction: column;
   height: 100vh;
 }
 .camera-video {
   display: flex;
- /*  flex-direction: column; */
   overflow-y: auto;
-  justify-content: center;
- flex-wrap: wrap;
 
+  justify-content: center;
+  flex-wrap: wrap;
+  width: 100%;
 }
 .camera-col {
   display: flex;
-   flex-direction: column;
+
   justify-content: center;
   align-items: center;
 
   margin-top: auto;
-  height: 280px;
-  width: 40%;
-  bottom: 0px;
+
+  width:90%;
+  flex-shrink: 0;
 
 }
 .cameraList {
   width: 100%;
+  display: flex;
+
+}
+.cameraList1{
+  width: 40vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+
 }
 .cameraSelect {
   width: 35%;
@@ -344,5 +346,4 @@ onMounted(() => {
   margin-top: 10px;
   margin-bottom: 10px;
 }
-
 </style>
--- src/components/CameraCard.vue
+++ src/components/CameraCard.vue
@@ -1,34 +1,41 @@
 <template>
-
-    <el-card id="videoCard" :class="{'selected-card':isSelected}" @click="selectThisCamera">
-      <div class="cameraText">预览窗口</div>
-      <canvas id="videoCanvas" ref="videoRef"></canvas>
-    </el-card>
-
+  <el-card
+    id="videoCard"
+    :class="{ 'selected-card': isSelected, 'zoom-in': isZoomed }"
+    @click="selectThisCamera"
+  >
+    <div class="cameraText">预览窗口</div>
+    <canvas id="videoCanvas" ref="videoRef"></canvas>
+    <div>
+      <i
+        class="iconfont2 icon2-zoomin"
+        @click.stop="toggleZoom"
+        style="font-size: x-large; cursor: pointer"
+      ></i>
+    </div>
+  </el-card>
 </template>
 <script setup lang="ts">
-
-import { ElCard} from "element-plus";
-import { onMounted, ref, inject, computed, onUnmounted} from "vue";
-import {
-  stopGrabImage,
-  getImage,closeCamera_
-} from "@/api/path";
-
+import { ElCard } from "element-plus";
+import { onMounted, ref, inject, computed, onUnmounted } from "vue";
+import { stopGrabImage, getImage, closeCamera_ } from "@/api/path";
+import "../assets/iconfont2/iconfont.css";
 const props = defineProps<{
-  cameraIndex:number,
-  cameraType:string,
-  cameraSN:string,
+  cameraIndex: number;
+  cameraType: string;
+  cameraSN: string;
 }>();
-const selectedIndex=inject("selectedIndex")
-const isSelected = computed(()=>{
-  return selectedIndex.value==props.cameraIndex
-})
-const selectThisCamera=function(){
-  selectedIndex.value=props.cameraIndex
-}
+const selectedIndex = inject("selectedIndex");
+const isSelected = computed(() => {
+  return selectedIndex.value == props.cameraIndex;
+});
+const selectThisCamera = function () {
+  selectedIndex.value = props.cameraIndex;
+};
 const videoRef = ref(null);
 let ctx;
+const dpr = window.devicePixelRatio || 1;
+const isZoomed = ref(false);
 let socket: WebSocket;
 const image = new Image(); // 创建一个 Image 对象
 
@@ -60,7 +67,6 @@ const openConnection = function () {
 
   // WebSocket 连接关闭
   socket.onclose = () => {
-
     console.log("WebSocket connection closed.");
   };
 };
@@ -72,8 +78,6 @@ const closeConnection = function () {
   stopGrabImage({ cameraType: props.cameraType, cameraSN: props.cameraSN });
 };
 
-
-
 const getSingleImage = function () {
   getImage({
     cameraType: props.cameraType,
@@ -82,41 +86,50 @@ const getSingleImage = function () {
     image.src = "data:image/jpeg;base64," + res.image;
   });
 };
+const toggleZoom = function () {
+  isZoomed.value = !isZoomed.value;
+  if (isZoomed.value) {
+    videoRef.value.style.width = `60vw`;
+    videoRef.value.style.height = `70vh`;
+  } else {
+    videoRef.value.style.width = `400px`;
+    videoRef.value.style.height = `250px`;
+  }
+};
 onMounted(() => {
   ctx = videoRef.value.getContext("2d");
-  const ratio = window.devicePixelRatio || 1;
 
   //videoRef.value.width = 400*ratio;
   //videoRef.value.height = 250*ratio;
-  videoRef.value.style.width=`400px`
-  videoRef.value.style.height=`250px`
- // ctx.setTransform(ratio, 0, 0, ratio, 0, 0);
- image.onload = drawImage
+  videoRef.value.style.width = `400px`;
+  videoRef.value.style.height = `250px`;
+  // ctx.setTransform(ratio, 0, 0, ratio, 0, 0);
+  image.onload = drawImage;
   image.src = "/preview.png";
-
-  ctx.scale(ratio, ratio);
-  window.addEventListener('beforeunload', sendRequestBeforeRefresh);
-
+  ctx.imageSmoothingEnabled = false;
+  window.addEventListener("beforeunload", sendRequestBeforeRefresh);
 });
 
-const drawImage=function(){
-  videoRef.value.width=image.naturalWidth;
-  videoRef.value.height=image.naturalHeight;
+const drawImage = function () {
+  // videoRef.value.width=image.naturalWidth*dpr;
+  //videoRef.value.height=image.naturalHeight*dpr;
+  //ctx.scale(dpr, dpr);
+  //ctx.drawImage(image, 0, 0, image.naturalWidth*dpr, image.naturalHeight*dpr);
+
+  videoRef.value.width = image.naturalWidth;
+  videoRef.value.height = image.naturalHeight;
   ctx.drawImage(image, 0, 0, image.naturalWidth, image.naturalHeight);
-
-}
-onUnmounted(()=>{
-  window.removeEventListener('beforeunload', sendRequestBeforeRefresh);
-
-})
+};
+onUnmounted(() => {
+  window.removeEventListener("beforeunload", sendRequestBeforeRefresh);
+});
 const sendRequestBeforeRefresh = function (event) {
- //event.preventDefault();
+  //event.preventDefault();
   //closeConnection();
   closeCamera_({
     cameraType: props.cameraType,
     cameraSN: props.cameraSN,
   });
-
 };
 defineExpose({
   getSingleImage,
@@ -125,28 +138,38 @@ defineExpose({
 });
 </script>
 <style scoped>
-
 .cameraText {
-  font-size: 1vw;
+  font-size: large;
   align-items: center;
-  margin-bottom: 2vh;
+  margin-bottom: 2%;
 }
 #videoCard {
   display: flex;
   flex-direction: column;
-  margin: 2px;
-  padding: 2px;
-  height: 350px;
-
+  justify-content: center;
+  align-items: center;
+  margin: 0.5%;
 }
 #videoCanvas {
   /* width: 100%;
   height: 60%; */
-  flex:1;
+  flex: 1;
   border: 2px #e8e6e6 solid;
 }
-.selected-card{
+.selected-card {
+  box-shadow: 0 0 10px rgba(0, 123, 255, 0.5); /* 蓝色外框阴影 */
+}
 
-  box-shadow: 0 0 10px rgba(0, 123, 255, 0.5);  /* 蓝色外框阴影 */
+.zoom-in {
+  transition: transform 0.1s ease-in-out, width 0.1s ease-in-out,
+    height 0.1s ease-in-out;
+  position: fixed; /* 固定在屏幕上 */
+
+  width: 70vw; /* 让它填满整个屏幕 */
+  height: 90vh;
+  z-index: 1000; /* 提高层级，确保在最上面 */
+}
+.zoom-in #videoCanvas {
+  border: none;
 }
 </style>
